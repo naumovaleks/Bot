ef1e354086d135b0923bb4f1a60987b19a88632a
naumovaleks <40571947+naumovaleks@users.noreply.github.com>
2018-07-12 18:33:00 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.loginNDY2NTE2NTg0MjY2MjAzMTQ3.DiixWw.KXDzirq6jsA8dDj4yGA0MFOaZns();
